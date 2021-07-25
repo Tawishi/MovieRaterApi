f3c104139dda7c6bd0dc3d642230db8c134c3c7d
Tawishi <tawishisharma1@gmail.com>
2021-07-25 09:42:56 +0530

    Created, registered models

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rest_framework import router
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 
