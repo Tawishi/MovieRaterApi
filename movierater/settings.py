af5e0cab405afcec54fcbff3db361e297846c7e0
Tawishi <tawishisharma1@gmail.com>
2021-08-09 16:57:31 +0530

    Ready for Deployment

--- movierater/settings.py
+++ movierater/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rest-movie-rater.herokuapp.com']
 
 
 # Application definition
