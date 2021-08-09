254a51da8153c1a46cc53bc5559692106e68a053
Tawishi <tawishisharma1@gmail.com>
2021-08-09 16:33:34 +0530

    Ready for Deployment

--- movierater/settings.py
+++ movierater/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'movierater.wsgi.application'
 default_dburl = 'sqlite///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-    'default': os.getenv('DATABASE_URL', default=default_dburl, cast=dburl),
+    'default': os.getenv('DATABASE_URL', default=default_dburl),
 }
 
 
