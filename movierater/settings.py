b4367e77ce101273c3ec732897ef980bcfe23cd7
Tawishi <tawishisharma1@gmail.com>
2021-08-09 17:40:57 +0530

    Fix in settings

--- movierater/settings.py
+++ movierater/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['rest-movie-rater.herokuapp.com']
+ALLOWED_HOSTS = ['rest-movie-rater.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
